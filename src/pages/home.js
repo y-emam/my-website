79150bfc9b9bd39c64122c06b211ee659cfbb0fc
Yasser Emam <yasseremam2002@gamil.com>
2023-03-22 18:32:59 +0200

    made the header of the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,14 @@
 import Header from "../components/header";
 import Navbar from "../components/navbar";
+import Portfolio from "../components/portfolio";
+import "../css/home.css";
 
 function HomePage() {
   return (
     <div className="home-page">
       <Navbar />
       <Header />
+      <Portfolio />
     </div>
   );
 }

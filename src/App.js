a97e2e2aa61836cc75a0ff81446858799a089086
Yasser Emam <yasseremam2002@gamil.com>
2023-03-24 15:56:57 +0200

    removed the nopage and i had already made the header section reaponsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import "./App.css";
 import HomePage from "./pages/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NoPage from "./pages/no-page";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -9,12 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          {/* <Route path="projects" element={<Projects />} />
-          <Route path="hire-me" element={<HireMe />} />
-          <Route path="contact" element={<Contacts />} />
-          <Route path="about" element={<About />} /> */}
-          <Route path="*" element={<NoPage />} />
+          <Route path="*" element={<HomePage />} />
         </Routes>
       </Router>
     </div>
--- src/pages/no-page.js
+++
@@ -1,5 +0,0 @@
-function NoPage() {
-  return <div className="no-page">NoPage</div>;
-}
-
-export default NoPage;

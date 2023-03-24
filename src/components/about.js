a5230c198c899aad223b4d966f9d100d0c2cc22d
Yasser Emam <yasseremam2002@gamil.com>
2023-03-24 03:43:01 +0200

    I need to make the header responsive and add css to the about section

---
+++ src/components/about.js
@@ -0,0 +1,25 @@
+function About() {
+  return (
+    <div className="about-section">
+      <div className="about-text">
+        <h2>
+          I'm <span>Yasser Emam</span>, Software Engineer
+        </h2>
+        <p>
+          Looking to take your business online? As a web and mobile app
+          developer, I can assist you in making that transition smoothly and
+          effectively.
+        </p>
+        <p>
+          Affordable digital solutions for your brand are just a click away with
+          my web and mobile app development services. My expertise lies in
+          connecting your brand to a wide audience, while ensuring a seamless
+          user experience
+        </p>
+      </div>
+      <div className="about-info"></div>
+    </div>
+  );
+}
+
+export default About;

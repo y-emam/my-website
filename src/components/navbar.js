2cf081b7d336b5f0adff66f42d8fda2b05c06231
Yasser Emam <yasseremam2002@gamil.com>
2023-03-25 18:09:23 +0200

    working on the bug of the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,25 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 function Navbar() {
+  const [isSideBarActive, setIsSideBarActive] = useState(false);
+
+  const sideBarClick = () => {
+    const sideBar = document.getElementById("side-icon");
+
+    if (isSideBarActive) {
+      sideBar.classList.add("active");
+      setIsSideBarActive(true);
+    } else {
+      sideBar.classList.remove("active");
+      setIsSideBarActive(false);
+    }
+  };
+
   return (
     <nav className="navbar ">
-      <label id="side-icon">
+      <label id="side-icon" onClick={sideBarClick}>
         <input type={"checkbox"} id="check" />
         <FontAwesomeIcon icon={faBars} className="checkbtn" />
         <div className="dropdown-content">
